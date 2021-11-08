9d441675c48547255fb934d5985b0917427e3847
Javiera <javiera.hormazabal9@gmail.com>
2021-11-08 01:05:20 -0300

    Cree activity_calificar_vet.xml

--- app/src/main/java/com/example/wof_javierahormazabal/perfil_act.java
+++ app/src/main/java/com/example/wof_javierahormazabal/perfil_act.java
@@ -3,12 +3,37 @@ package com.example.wof_javierahormazabal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class perfil_act extends AppCompatActivity {
+
+    public TextView name, type, age;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+    }
+
+    public void Vacunas(View view)
+    {
+
+    }
+
+    public void Consultas (View view)
+    {
+
+    }
+
+    public void Tratamientos (View view)
+    {
+
+    }
+
+    public void Calificar (View view)
+    {
+
     }
 }
